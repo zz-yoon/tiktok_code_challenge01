8146d3c632581b4fb4d57898e52d6a4d3592458f
zz-yoon <yun.zz.del@gmail.com>
2025-02-01 17:22:10 +0900

    #5.4 animatedCrossFade

--- lib/screens/onboarding/tutorial_screen2.dart
+++ lib/screens/onboarding/tutorial_screen2.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_code_challenge01/constants/gaps.dart';
 import 'package:tiktok_code_challenge01/constants/sizes.dart';
+
+enum Direction { right, left }
+
+enum Page { first, second }
 
 class TutorialScreen2 extends StatefulWidget {
   const TutorialScreen2({super.key});
@@ -10,16 +15,39 @@ class TutorialScreen2 extends StatefulWidget {
 }
 
 class _TutorialScreen2State extends State<TutorialScreen2> {
-  void _onPanUpdate(DragDownDetails details) {
+  Direction _direction = Direction.right;
+  Page _showPage = Page.first;
+
+  void _onPanUpdate(DragUpdateDetails details) {
     print(details);
     if (details.delta.dx > 0) {
-    } else {}
+      setState(() {
+        _direction = Direction.right;
+      });
+    } else {
+      setState(() {
+        _direction = Direction.left;
+      });
+    }
+  }
+
+  void _onPanEnd(DragEndDetails detail) {
+    if (_direction == Direction.left) {
+      setState(() {
+        _showPage = Page.second;
+      });
+    } else {
+      setState(() {
+        _showPage = Page.first;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onPanUpdate: _onPanUpdate,
+      onPanEnd: _onPanEnd,
       child: SafeArea(
         child: Scaffold(
           body: Padding(
@@ -61,8 +89,30 @@ class _TutorialScreen2State extends State<TutorialScreen2> {
                   ),
                 ],
               ),
-              crossFadeState: CrossFadeState.showFirst,
+              crossFadeState: _showPage == Page.first
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
               duration: Duration(milliseconds: 300),
+            ),
+          ),
+          bottomNavigationBar: SafeArea(
+            child: BottomAppBar(
+              child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: Sizes.size1,
+                    horizontal: Sizes.size1,
+                  ),
+                  child: AnimatedOpacity(
+                    duration: Duration(milliseconds: 3),
+                    opacity: _showPage == Page.first ? 0 : 1,
+                    child: CupertinoButton(
+                        child: Text("Enter the app!",
+                            style: TextStyle(
+                              color: Colors.white,
+                            )),
+                        onPressed: () {},
+                        color: Theme.of(context).primaryColor),
+                  )),
             ),
           ),
         ),
